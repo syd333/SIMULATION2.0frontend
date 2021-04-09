3e1c10a577ab663d587ec936aab863fc008955d1
syd333 <sydmiranda@icloud.com>
2021-04-09 15:20:09 -0700

    added font style and api.js

---
+++ src/services/Api.js
@@ -0,0 +1,11 @@
+const SIMULATION = `http://localhost:3000`;
+
+const token = () => localStorage.getItem("token");
+
+const headers = () => {
+  return {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: token(),
+  };
+};
