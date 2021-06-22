f3cede188b577602e1307a0da7a087062c342069
syd333 <sydmiranda@icloud.com>
2021-06-22 15:00:25 -0400

    adding hooks + setting up functional component to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { withRouter, Route, Switch } from "react-router-dom";
 import { api } from "./services/Api";
 import { connect } from "react-redux";
@@ -12,10 +12,33 @@ import HomeContainer from "./components/Containers/HomeContainer";
 import MissContainer from "./components/Containers/MissContainer";
 import SingleMissPage from "./components/Miss/SingleMissPage";
 import CreateMissButton from "./components/CreateMissButton";
-import CreateMiss from './components/Miss/CreateMiss';
-import Search from './components/Account/Search';
-import Favorites from './components/Account/Favorites';
-import CreateReply from './components/Reply/CreateReply';
+import CreateMiss from "./components/Miss/CreateMiss";
+import Search from "./components/Account/Search";
+import Favorites from "./components/Account/Favorites";
+import CreateReply from "./components/Reply/CreateReply";
+
+// const App = () => {
+//   const [auth, setAuth] = useState({ user: {} });
+// useEffect(() => {
+ // const token = localStorage.token;
+  // if (token && token != "undefined") {
+    // api.auth.getCurrentUser().then((data) => {
+      //setAuth({
+        //user: {
+          //id: data.user.id,
+          //name: data.user.name,
+//         },
+//       });
+//       setLocationTesting({
+//         location: {
+//           latitude: data.user.location.latitude,
+//           longitude: data.user.location.longitude,
+//         },
+//       });
+//     });
+//   }
+// }, []);
+// }
 
 class App extends Component {
   componentDidMount() {
@@ -23,7 +46,7 @@ class App extends Component {
     if (token) {
       api.auth.getCurrentUser().then((data) => {
         this.props.Auth(data);
-        this.props.getFavorites(data.user.favorites)
+        this.props.getFavorites(data.user.favorites);
       });
     }
   }
@@ -31,8 +54,6 @@ class App extends Component {
     localStorage.removeItem("token");
     this.props.Auth({ user: {} });
   };
-
-
 
   render() {
     return (
@@ -44,12 +65,12 @@ class App extends Component {
           <Route path="/signup" exact component={SignUpPage} />
           <Route path="/misses" exact component={MissContainer} />
           <Route path="/miss" exact component={SingleMissPage} />
-          <Route path="/misses/new" exact component={CreateMiss}/>
+          <Route path="/misses/new" exact component={CreateMiss} />
           <Route path="/search" exact component={Search} />
-          <Route path="/favorites" exact component={Favorites}/>
+          <Route path="/favorites" exact component={Favorites} />
           <Route path="/replyback" exact component={CreateReply} />
         </Switch>
-        <CreateMissButton onCreate={this.onCreate}/>
+        <CreateMissButton onCreate={this.onCreate} />
       </div>
     );
   }
@@ -59,4 +80,6 @@ const mapStateToProps = (state) => {
   return {};
 };
 
-export default connect(mapStateToProps, { Auth, addMiss, getFavorites })(withRouter(App));
+export default connect(mapStateToProps, { Auth, addMiss, getFavorites })(
+  withRouter(App)
+);
